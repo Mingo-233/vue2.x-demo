a9b4acdc3b17fa155a80bb3fe412b95e714185b6
mingo <mingo233@qq.com>
2021-10-12 18:15:59 +0800

    feat:functional components

---
+++ src/components/nr-tip.vue
@@ -0,0 +1,30 @@
+<script>
+export default {
+  name: 'nr-tip',
+  functional: true,
+  props: {
+    iconClass: {
+      type: String,
+      default: 'el-icon-info'
+    },
+    content: {
+      type: String,
+      default: ''
+    }
+  },
+  render (h, context) {
+    const { iconClass, content } = context.props
+    const vnodes = []
+    vnodes.push(
+      <el-tooltip class="item"
+        effect="dark"
+        content={content}
+        placement="top-start">
+        <i class={iconClass}
+          style="color:#BABCBF;width:4px"></i>
+      </el-tooltip>
+    )
+    return vnodes
+  }
+}
+</script>
